190e5efcf21be2b931f5467c55499383173679d8
plthompson <patrickthompson9@gmail.com>
2019-09-19 11:11:58 -0700

    Adding scripts for the function that runs the simulations and the script that produces the figures

---
+++ scripts/BEF_figures.R
@@ -0,0 +1,331 @@
+library(tidyverse)
+library(cowplot)
+library(vegan)
+library(minpack.lm)
+
+source("./scripts/BEF_scale_functions.R")
+
+colV <- c("grey", "pink1", "red")
+
+#Figure 2#####
+sp_0 <- BEF_simulation(type = "spatial" ,env_gamma = 0)
+env_sp0<-(decostand(c(sp_0[[2]][1,],-0.2,1.2),method = "range")[1:80])*100
+
+sp_1 <- BEF_simulation(type = "spatial" ,env_gamma = 1)
+env_sp1 <- (decostand(c(sp_1[[2]][1,],-0.2,1.2),method = "range")[1:80])*100
+
+sp_2 <- BEF_simulation(type = "spatial" ,env_gamma = 2)
+env_sp2 <- (decostand(c(sp_2[[2]][1,],-0.2,1.2),method = "range")[1:80])*100
+
+tp_0 <- BEF_simulation(type = "temporal" ,env_gamma = 0)
+env_tp0 <- (decostand(c(tp_0[[2]][81:160],-0.2,1.2),method = "range")[1:80])*100
+
+tp_1 <- BEF_simulation(type = "temporal" ,env_gamma = 1)
+env_tp1 <- (decostand(c(tp_1[[2]][81:160],-0.2,1.2),method = "range")[1:80])*100
+
+tp_2 <- BEF_simulation(type = "temporal" ,env_gamma = 2)
+env_tp2 <- (decostand(c(tp_2[[2]][81:160],-0.2,1.2),method = "range")[1:80])*100
+
+Fig2a<- data.frame(N = c(sp_0[[1]][,,100]), species = rep(1:100, each = 80), patch = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = patch, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_sp0, patch = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 0")+
+  ylim(0,100)
+
+Fig2b<- data.frame(N = c(sp_1[[1]][,,100]), species = rep(1:100, each = 80), patch = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = patch, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_sp1, patch = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 1")+
+  ylim(0,100)
+
+Fig2c<- data.frame(N = c(sp_2[[1]][,,100]), species = rep(1:100, each = 80), patch = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = patch, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_sp2, patch = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 2")+
+  ylim(0,100)
+
+Fig2d<- data.frame(N = c(tp_0[[1]][81:160,,100]), species = rep(1:100, each = 80), time = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = time, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_tp0, time = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 0")+
+  ylim(0,100)
+
+Fig2e<- data.frame(N = c(tp_1[[1]][81:160,,100]), species = rep(1:100, each = 80), time = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = time, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_tp1, time = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 1")+
+  ylim(0,100)
+
+Fig2f<- data.frame(N = c(tp_2[[1]][81:160,,100]), species = rep(1:100, each = 80), time = 1:80) %>%
+  filter(N >0) %>% 
+  ggplot(aes(x = time, y = species, size = N))+
+  geom_point()+
+  ylab("species ID")+
+  scale_size_continuous(range = c(0.1,1),guide = F, breaks = c(0.05,1,1.4))+
+  geom_line(data = data.frame(species = env_tp2, time = 1:80), aes(size = NULL),color = "red", size = 0.5, alpha = 1)+
+  ggtitle("gamma = 2")+
+  ylim(0,100)
+
+plot_grid(Fig2a, Fig2b, Fig2c, Fig2d, Fig2e, Fig2f, nrow = 2, labels = "AUTO")
+ggsave("./figures/Fig.2.png", width = 10*1.2, height = 6*1.2)
+
+
+#run simulations####
+reps <- 100
+hold.raw.data <-data.frame()
+coef.df <- data.frame()
+for(r in 21:reps){
+  for(gamma in c(0,1,2)){
+    print(paste("rep = ",r,"; gamma = ", gamma, sep = ""))
+    spatial_run <- BEF_simulation(type = "spatial", env_gamma = gamma)
+    spatial_com <- spatial_run[[1]]
+    temporal_run <- BEF_simulation(type = "temporal", env_gamma = gamma)
+    temporal_com <- temporal_run[[1]][81:160,,]
+    
+    hold.data.run<-data.frame()
+    for(k in c(1:30,32,34,36,38,40,45,50,55,60,65,70,75,80)){
+      hold.data<-data.frame()
+      for(j in 1:100){
+        if(k == 1){
+          div_space<-renyi(spatial_com[1:k,,j],scales = 0:1,hill = TRUE)
+          div_time<-renyi(temporal_com[1:k,,j],scales = 0:1,hill = TRUE)
+        } else{
+          div_space<-renyi(colSums(spatial_com[1:k,,j]),scales = 0:1,hill = TRUE)
+          div_time<-renyi(colSums(temporal_com[1:k,,j]),scales = 0:1,hill = TRUE)
+        }
+        bmass_space<-sum(spatial_com[1:k,,j])
+        bmass_time<-sum(temporal_com[1:k,,j])
+        
+        hold.data<-bind_rows(hold.data,data.frame(SR = c(div_space[1], div_time[1]), D = c(div_space[2], div_time[2]), bmass = c(bmass_space, bmass_time), scale = k, sp.pool = j, scenario = c("space", "time")))
+        hold.data$gamma<-gamma
+        hold.data$rep<-r
+      }
+      coef.hold_space <- coef(nlsLM(formula = bmass ~ (a * SR)/ (SR + b), data = filter(hold.data, scenario == "space"),start = c(a = max(hold.data$bmass), b = max(hold.data$bmass)/2)))
+      coef.hold_time <- coef(nlsLM(formula = bmass ~ (a * SR)/ (SR + b), data = filter(hold.data, scenario == "time"), start = c(a = max(hold.data$bmass), b = max(hold.data$bmass)/2)))
+      coef.hold.df <- bind_rows(coef.hold_space,coef.hold_time)
+      coef.hold.df$scenario = c("space", "time")
+      coef.hold.df$rep <- r
+      coef.hold.df$scale <- k
+      coef.hold.df$gamma <- gamma
+      coef.df<-bind_rows(coef.df, coef.hold.df)
+      
+      hold.raw.data <- rbind(hold.raw.data, hold.data)
+    }
+  }
+}
+
+
+
+#Figure 3####
+beta.df <- hold.raw.data %>% 
+  filter(sp.pool == 100) %>% 
+  group_by(rep, scenario, gamma) %>% 
+  mutate(SR_alpha = first(SR)) %>% 
+  group_by(rep, scenario, gamma, scale) %>% 
+  mutate(beta = SR/SR_alpha) %>% 
+  ungroup() %>%
+  group_by(scenario, gamma, scale) %>% 
+  summarise(lower = quantile(beta, probs = 0.25), upper = quantile(beta, probs = 0.75), beta = median(beta))
+
+Fig.3a<- beta.df %>% 
+  filter(scenario == "space") %>% 
+  ggplot(aes(x=scale,y=beta, color = factor(gamma), group = gamma, fill = factor(gamma)))+
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, col = NA)+
+  geom_line(size = 1)+
+  scale_color_manual(values = colV, name = expression(paste("environmental ", gamma, sep = "")))+
+  scale_fill_manual(values = colV, guide = F)+
+  theme_classic()+
+  xlab("spatial scale (# of local patches)")+
+  ylab(expression(paste("spatial ", beta, "-diversity", sep = "")))+
+  theme(legend.justification=c(1,0), legend.position=c(1,0.001))+
+coord_cartesian(ylim = c(beta.df$lower, beta.df$upper))
+
+Fig.3b<- beta.df %>% 
+  filter(scenario == "time") %>% 
+  ggplot(aes(x=scale,y=beta, color = factor(gamma), group = gamma, fill = factor(gamma)))+
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, col = NA)+
+  geom_line(size = 1)+
+  scale_color_manual(values = colV, name = "gamma", guide = F)+
+  scale_fill_manual(values = colV, guide = F)+
+  theme_classic()+
+  xlab("temporal scale (# of time steps)")+
+  ylab(expression(paste("temporal ", beta, "-diversity", sep = "")))+
+  theme(legend.justification=c(1,0), legend.position=c(1,0.01))+
+  coord_cartesian(ylim = c(beta.df$lower, beta.df$upper))
+
+plot_grid(Fig.3a, Fig.3b, labels = "AUTO")
+ggsave("./figures/Fig.3.png", height = 4, width = 9) 
+
+#Figure 4####
+b.df <- data.frame()
+for(i in c(1,2,3,5,10,20,30,40,60,80)){
+  for(g_sel in c(0,1,2)){
+    for(scenario in c("space", "time")){
+    hold.data = filter(hold.raw.data, rep == 2, gamma == g_sel, scale == i, scenario == scenario)
+    b.df <- rbind(b.df, data.frame(b = coef(nlsLM(formula = bmass ~ (a * SR)/ (SR + b), 
+                                                  data = hold.data, 
+                                                  start = c(a = max(hold.data$bmass), b = max(hold.data$bmass)/2)))[2], 
+                                   scale = i,
+                                   gamma = g_sel,
+                                   scenario = scenario))
+  }}}
+
+Fig.4a <- hold.raw.data %>%
+  filter(rep == 2, gamma == 0, scenario == "space") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 0, scenario=="space"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 0")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+Fig.4b <- hold.raw.data %>%
+  filter(rep == 2, gamma == 1, scenario == "space") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 1, scenario=="space"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 1")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+Fig.4c <- hold.raw.data %>%
+  filter(rep == 2, gamma == 2, scenario == "space") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 2, scenario=="space"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 2")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+Fig.4d <- hold.raw.data %>%
+  filter(rep == 7, gamma == 0, scenario == "time") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 0, scenario=="time"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 0")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+Fig.4e <- hold.raw.data %>%
+  filter(rep == 1, gamma == 1, scenario == "time") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 1, scenario=="time"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 1")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+Fig.4f <- hold.raw.data %>%
+  filter(rep == 2, gamma == 2, scenario == "time") %>%
+  filter(scale %in% c(1,2,3,5,10,20,30,40,60,80)) %>% 
+  ggplot(aes(x = SR, y = bmass/scale, fill = scale, group = scale))+
+  scale_fill_viridis_c(end = 0.8, option = "B", name = "temporal\nscale", guide = FALSE, trans = "log10")+
+  ylab("cumulative biomass/scale")+
+  xlab("species richness")+
+  geom_vline(data = filter(b.df, gamma == 2, scenario=="time"), aes(xintercept = b, color = factor(scale), fill = NULL), linetype = 2)+
+  geom_point(pch = 21, size = 2.5)+
+  geom_smooth(method = "nls", 
+              formula = y ~ a * x / (b + x), se = FALSE, aes(color = factor(scale)))+
+  scale_color_viridis_d(end = 0.8, option = "B", guide = FALSE)+
+  scale_linetype(guide = FALSE)+
+  ggtitle("gamma = 2")+
+  ylim(c(0,max(hold.raw.data$bmass)/max(hold.raw.data$scale)))+
+  xlim(c(0,100))
+
+plot_grid(Fig.4a, Fig.4b, Fig.4c, Fig.4d, Fig.4e, Fig.4f, labels = "AUTO",nrow = 2)
+ggsave("./figures/Fig.4.png", height = 10, width = 16)
+
+#Figure 5####
+sum.coef <- coef.df %>% 
+  group_by(scenario, scale, gamma) %>% 
+  summarise(lower = quantile(b, probs = 0.25), upper = quantile(b, probs = 0.75), b = median(b)) 
+
+Fig.5a <- sum.coef %>%
+  filter(scenario == "space") %>% 
+  ggplot(aes(x = scale,y = b, color = factor(gamma), group = gamma, fill = factor(gamma)))+
+  geom_hline(yintercept = filter(sum.coef, scale == 1, scenario == "space")$b, color = colV, lty = 2)+
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, col = NA)+
+  geom_line()+
+  scale_color_manual(values = colV, name = expression(paste("environmental ", gamma, sep = "")))+
+  scale_fill_manual(values = colV, guide = F)+
+  theme_classic()+
+  xlab("temporal scale (# of patches)")+
+  ylab(expression(paste("BEF half saturation richness (", italic(b[i]),")"), sep = ""))+
+  coord_cartesian(ylim = c(sum.coef$lower, sum.coef$upper))+
+  theme(legend.justification=c(1,0), legend.position=c(1,0.001))
+
+Fig.5b <- sum.coef %>%
+  filter(scenario == "time") %>% 
+  ggplot(aes(x = scale,y = b, color = factor(gamma), group = gamma, fill = factor(gamma)))+
+  geom_hline(yintercept = filter(sum.coef, scale == 1, scenario == "time")$b, color = colV, lty = 2)+
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, col = NA)+
+  geom_line()+
+  scale_color_manual(values = colV, name = "gamma", guide = F)+
+  scale_fill_manual(values = colV, guide = F)+
+  theme_classic()+
+  xlab("temporal scale (# of time steps)")+
+  ylab(expression(paste("BEF half saturation richness (", italic(b[i]),")"), sep = ""))+
+  coord_cartesian(ylim = c(sum.coef$lower, sum.coef$upper))
+
+plot_grid(Fig.5a, Fig.5b, labels = "AUTO")
+ggsave("./figures/Fig.5.png", height = 4, width = 9) 
---
+++ scripts/BEF_scale_functions.R
@@ -0,0 +1,72 @@
+library(synchrony)
+
+BEF_simulation <- function(type = "spatial", env_gamma = 2){
+  #fixed parameters
+  species <- 100
+  sigma <- 0.25 #environmental niche breadth
+  r_max <- 4 #max growth rate
+  alpha <- matrix(runif(n = species*species, min = 0, max = 0.25), species, species) #per capita interspecific competition matrix
+  diag(alpha) <- 1 #per capita intraspecific competition
+  z <- seq(-0.2, 1.2, length = species) #environmental optima
+  ext_thresh <- 0.05 #extinction threshold
+  reseed_value <- 0.035
+  
+  if(type == "spatial"){
+    Tmax <- 150 #length of simulation
+    patches <- 80
+    #environment
+    env.df<-data.frame(env = phase.partnered(n = patches, gamma = env_gamma, mu = 0.5, sigma = 0.25)$timeseries[,1], patch = 1:patches)
+    #species responses to environmental variation
+    r <- matrix(r_max*exp(-((rep(z, each = patches)-env.df$env)/(2*sigma))^2), patches, species)
+  }
+  
+  
+  if(type == "temporal"){
+    env_time <- 80
+    Tmax <- env_time*2 #length of simulation
+    patches <- 1
+    #environment
+    env.hold<-phase.partnered(n = env_time, gamma = env_gamma, mu = 0.5, sigma = 0.25)$timeseries[,1]
+    env.df <- data.frame(env = c(env.hold,rev(env.hold)), time = 1:Tmax)
+  }
+  
+  if(type == "spatial"){
+    Nsave_all <- array(NA,dim=c(patches,species,species))
+  }
+  if(type == "temporal"){
+    Nsave_all <- array(NA,dim=c(Tmax,species,species))
+  }
+  pb <- txtProgressBar(min = 0, max = species, style = 3)
+  for(j in 1:species){
+    N<-matrix(0,ncol=species,nrow=patches) #empty matrix of abundances
+    seed.sp <- sample(1:species,size = j,replace = F) #choose which species to include
+    N[,seed.sp]<-1 #seed initial abundances
+    
+    #community dynamics
+    Nsave<-array(NA,dim=c(patches,species,Tmax))
+    Esave<-matrix(NA,nrow = Tmax, ncol = patches)
+    
+    for(i in 1:Tmax){
+      if(type == "temporal"){
+        r <- r_max*exp(-((z-env.df$env[i])/(2*sigma))^2)
+      } 
+      if (type == "spatial") {
+        Esave[i,]<-env.df$env
+      }
+      N[,seed.sp][N[,seed.sp]==0] <- reseed_value
+      N <- N*r/(1+N%*%alpha) 
+      N[N<ext_thresh] <- 0 #apply extinction threshold
+      Nsave[,,i]<-N
+    }
+    if(type == "spatial"){
+      Nsave_all[,,j] <- N
+    }
+    if(type == "temporal"){
+      Esave[,1]<-env.df$env
+      Nsave_all[,,j] <- t(Nsave[1,,])
+    }
+    setTxtProgressBar(pb, j)
+  }
+  close(pb)
+  return(list(Nsave_all,Esave))
+}
